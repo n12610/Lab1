bc64472dd2d83738b6e28dc0a941677411d14b8c
Nick Williams <twilliams027@sdccd.edu>
2025-02-04 11:58:03 -0800

    Initial sharing of project

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+/**
+ * This is my first java app. It prints out Hello
+ *World to the console 
+ * @author Nick Williams, twilliams027@student.sdccd.edu
+ * @since 2/4/2025, Nick Williams
+ * @version v1.0
+ */
+
+public class HelloWorld
+{
+ public static void main(String[] args) 
+ {
+     System.out.println("Hellow world!");
+     System.out.println("Nick Williams");
+    }
+ }
