dca756f0c36bb722b51625afe1e5089dd872cb5c
n12610 <twilliams027@student.sdccd.edu>
2025-02-06 10:52:06 -0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -10,7 +10,7 @@ public class HelloWorld
 {
  public static void main(String[] args) 
  {
-     System.out.println("Hellow world!");
+     System.out.println("Hello world!");
      System.out.println("Nick Williams");
     }
  }
